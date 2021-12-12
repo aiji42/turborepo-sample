e63d1f67dcdef432fbeb9e07f87dfa0160a7eda7
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-12-12 21:00:00 +0900

    add util

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,9 +1,10 @@
 import { Button } from "ui";
+import { bigNumberToString } from "utils"
 
 export default function Docs() {
   return (
     <div>
-      <h1>Docs updated</h1>
+      <h1>Docs {bigNumberToString(10000000000)}</h1>
       <Button />
     </div>
   );
---
+++ packages/utils/formatter.ts
@@ -0,0 +1,1 @@
+export const bigNumberToString = (n: number) => n.toLocaleString()
